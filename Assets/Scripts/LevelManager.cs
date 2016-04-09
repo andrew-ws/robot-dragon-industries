f8291dffbee80e2fecd8e52e77c787acd7b9eef8
Andrew Sherman <asherman@oberlin.edu>
2016-04-09 18:31:00 -0400

    Basic framework for moving and shooting

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+    public Player player;
+
+    public int aggro = 1;
+
 	// Use this for initialization
 	void Start () {
-	
+        this.transform.position = Vector3.zero;
+
+        player = (new GameObject()).AddComponent<Player>();
+        player.gameObject.name = "Player";
+        player.gameObject.transform.localPosition = Vector3.zero;
 	}
+
+    public void init(int whichLevel)
+    {}
 	
 	// Update is called once per frame
 	void Update () {
