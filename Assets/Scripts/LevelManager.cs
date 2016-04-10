cfb4cf8709964962e62bdcb80b2a0c6dff210c39
nwarford <noel.warford95@gmail.com>
2016-04-10 16:36:59 -0400

    setting up inital music stuff

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
     public Player player;
+	public MusicManager music;
+	public GameManager manager;
+	// I need our main GameManager to be passed along into MusicManager to access the audio
+	// that is attached to it
 
     public int aggro = 1;
 
@@ -14,13 +18,22 @@ public class LevelManager : MonoBehaviour {
         player = (new GameObject()).AddComponent<Player>();
         player.gameObject.name = "Player";
         player.gameObject.transform.localPosition = Vector3.zero;
+
 	}
 
-    public void init(int whichLevel)
-    {}
+	public void init(int whichLevel, GameManager manager)
+    {
+		this.manager = manager;
+
+		music = (new GameObject ()).AddComponent<MusicManager> ();
+		music.gameObject.name = "Music";
+		music.init(whichLevel, manager);
+	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
 }
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	private int level;
+	private GameManager manager;
+
+	// These are the objects that are attached to the 
+	public AudioSource music;
+	public AudioSource sfx;
+
+
+	public void init (int whichLevel, GameManager manager) {
+		this.level = whichLevel;
+		this.manager = manager;
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (level == 1) {
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void drop() {
+
+	}
+}
