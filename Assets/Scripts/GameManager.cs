c5922173e30b3b782967d872bebb6d9669bb412f
nwarford <noel.warford95@gmail.com>
2016-04-21 12:15:59 -0400

    making music programmatically

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,18 @@ public class GameManager : MonoBehaviour {
     public LevelManager lm;
 
 	// Need these for accessing music objects
-	public AudioSource music;
-	public AudioSource sfx;
+	public AudioSource[] sources;
+	private AudioSource source0;
+	private AudioSource source1;
+	private AudioSource source2;
+	private AudioSource source3;
 
-	private AudioClip intense_music;
-	private AudioClip nonintense_music;
+	private AudioClip nonintense_track;
+	private AudioClip drum_track;
+	private AudioClip intense_track;
 
+	public AudioMixer master;
+	public AudioMixer nonintense_mixer;
 	public AudioMixerSnapshot nonintense;
 	public AudioMixerSnapshot drums;
 	public AudioMixerSnapshot intense;
@@ -30,10 +36,7 @@ public class GameManager : MonoBehaviour {
         // Passing in for accessing some music objects in the scene
         lm.init(1, this);
 
-        // Music
-        /*intense_music = Resources.Load<AudioClip>("Music/Intense Loop");
-		nonintense_music = Resources.Load<AudioClip>("Music/Non-Intense Loop");
-		PlayMusic (nonintense_music);*/
+		manageAudio ();
 
 	}
 	
@@ -60,7 +63,7 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-	public void PlayMusic(AudioClip clip)
+	/*public void PlayMusic(AudioClip clip)
 	{
 		music.loop = true;
 		music.clip = clip;
@@ -71,7 +74,7 @@ public class GameManager : MonoBehaviour {
 	{
 		sfx.clip = clip;
 		sfx.Play();
-	}
+	}*/
 
 	public void drop() {
 		dropped = true;
@@ -83,5 +86,33 @@ public class GameManager : MonoBehaviour {
 		dropped = false;
 		drums.TransitionTo (0.01f);
 		lm.undrop ();
+	}
+
+	public void manageAudio() {
+		// Music
+		intense_track = Resources.Load<AudioClip>("Music/Intense Loop");
+		nonintense_track = Resources.Load<AudioClip>("Music/Non-Intense Loop");
+		drum_track = Resources.Load<AudioClip>("Music/Drums");
+
+		master = Resources.Load<AudioMixer> ("Music/Master");
+		nonintense_mixer = Resources.Load<AudioMixer> ("Music/Non-Intense");
+		nonintense = master.FindSnapshot ("Nonintense");
+		intense = master.FindSnapshot ("Intense");
+		drums = master.FindSnapshot ("Drums");
+
+		sources = gameObject.GetComponents<AudioSource> ();
+		source0 = sources [0];
+		setAudioSource (source0, nonintense_track, true);
+		source1 = sources [1];
+		setAudioSource (source1, drum_track, true);
+		source2 = sources [2];
+		setAudioSource (source2, intense_track, true);
+
+	}
+
+	private void setAudioSource(AudioSource source, AudioClip clip, bool loop) {
+		source.clip = clip;
+		source.playOnAwake = true;
+		source.loop = loop;
 	}
 }
